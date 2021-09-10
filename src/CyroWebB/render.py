62b44d0e4b7ac7b42531f12749243b058398ff50
sushant <github@cyrocoders.ml>
2021-09-10 13:37:44 +0530

    Fixed Templater

--- src/CyroWebB/render.py
+++ src/CyroWebB/render.py
@@ -2,14 +2,11 @@ import os
 from jinja2 import Environment, FileSystemLoader
 
 class Template:
-    def __init__(self, f="templates"):
-        self.templates_env = Environment(loader=FileSystemLoader(os.path.abspath(f)))
+    def __init__(self,server,f="templates"):
+        self.templates_env = Environment(loader=FileSystemLoader(os.path.join(os.path.dirname(os.path.abspath(server.context)),f)))
 
     def __call__(self, file, data=None):
         if data is None:
             data = {}
             
         return self.templates_env.get_template(file).render(**data)
-
-        with open(file) as f:
-            return f.read()
