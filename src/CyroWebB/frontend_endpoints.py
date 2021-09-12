85a81fe73477e0bd18dac04c8197cdc8e5037d1e
sushant <github@cyrocoders.ml>
2021-09-12 13:44:48 +0530

    Removed Unneccessary Request Parameter

--- src/CyroWebB/frontend_endpoints.py
+++ src/CyroWebB/frontend_endpoints.py
@@ -1,4 +1,4 @@
 def create(server):
     @server.create_endpoint("/icon/{name}.svg")
-    def icons(req,res,name):
+    def icons(res,name):
         server.get_external(res,f"https://cdn.jsdelivr.net/gh/CyroCoders/CyroWebF/icon/{name}.svg")
